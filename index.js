370bc67feda392db9ce5c0822c5a99b95f4768cf
Hamza <hamzaclans2001@gmail.com>
2026-02-24 10:48:15 +0500

    Export App as default in index.js to ensure compatibility with Snack rendering.

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ import App from './App';
 
 // boot the app (expo handles the rest)
 registerRootComponent(App);
+
+// Snack expects a default export from the entry file to render
+export default App;
